9b7f551519dd8bc51565964c9be01eee3425d6f8
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2021-11-08 16:37:07 +0000

    started adding products to admin and products page

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    mainImage_url = models.URLField(max_length=1254, null=True, blank=True)
+    Image_url2 = models.URLField(max_length=1254, null=True, blank=True)
+    Image_url3 = models.URLField(max_length=1254, null=True, blank=True)
+    main_image = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
