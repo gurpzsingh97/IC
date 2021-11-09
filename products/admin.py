70e00947f088a3c216cdaae42c0bdf775f77b168
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2021-11-09 13:51:01 +0000

    improved the admin page and added more products

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=(
+        'name',
+        'category',
+        'price',
+        'main_image',
+        'rating',
+    )
+    ordering = ('id',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=(
+        'name',
+        'friendly_name'
+    )
+    ordering = ('id',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/views.py
+++ products/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
+    categories = None
+    query = None
+    if request.GET:
+        if 'categories' in request.GET:
+            categories = request.GET['categories'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"you didnt enter any search criteria")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+    
     context = {
-        'products' : products
+        'products' : products,
+        'current_categories':categories,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
