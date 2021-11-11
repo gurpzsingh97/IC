06c3559e2e1a96139a6ce3a56656efaffe45978e
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2021-11-11 19:19:36 +0000

    added bad app and funtionality

---
+++ bag/contexts.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from django.conf import settings
+
+from IC.settings import FREE_DELIVERY_THRESHOLD
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count= 0
+    
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'total': total,
+        'bag_items': bag_items,
+        'delivery':delivery,
+        'product_count': product_count,
+        'free_delivery_delta': free_delivery_delta,
+        'grand_total': grand_total,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD
+    }
+
+    return context
