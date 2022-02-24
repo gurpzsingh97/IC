90f2d0a6088038287afa5bfbe62ec0de6c94c049
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2022-02-24 23:26:03 +0000

    created forms for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -47,7 +47,7 @@ class Order(models.Model):
 
 
 
-class OrderClassItem(models.Model):
+class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
