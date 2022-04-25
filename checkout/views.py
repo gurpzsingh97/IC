b618dcdfb14dd3dad63b653aa2e51e49e04685c1
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2022-04-25 13:36:27 +0100

    minor changes

--- checkout/views.py
+++ checkout/views.py
@@ -11,8 +11,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form':order_form
-    }
+        'order_form':order_form,
+        'stripe_public_key': 'pk_test_51KWl26Emg8ueC1eqp3DpFyfnhhd4iD0I1Ymd60002m2WHGE4flw1ccu3QIPEz4zNutN6J86Sg4bTy9eoRojBY6Rx00p2IKcaHR',
+        'client_secret':'sk_test_51KWl26Emg8ueC1eqDzFQJVzpgCaTwAw27md5J8dTkZ1WX11cojSHEV2X0xIEHQ4ocVzkPFCe1cPjVsKXlaXIZZKt004MALUlY1'
+    } 
 
     return render(request, template, context)
-
